45bcd1e9e61666c2ced9b951d64bbdb50d7ef42a
0xbbPizza <0xbbgo@gmail.com>
2022-02-27 22:08:25 +0800

    support muti domain

--- contracts/DestinationContract.sol
+++ contracts/DestinationContract.sol
@@ -5,6 +5,10 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "./Data.sol";
 
 interface IDestinationContract{
+
+    struct DestDomainStruct{
+        
+    }
     
     struct HashOnionFork{
         bytes32 onionHead;
--- contracts/SourceContract.sol
+++ contracts/SourceContract.sol
@@ -3,109 +3,88 @@
 pragma solidity 0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "./Data.sol";
-
-
-interface IArbSys { 
-    // from arbitrum
-    function sendTxToL1(address destAddr, bytes calldata calldataForL1) external payable;
-}
 
 interface ISourceContract{
-    // !!! for test , if release, dont need event
+    
+    struct DomainStruct{
+        uint256 txIndex;
+        bytes32 hashOnion;
+        bytes32 bringHashOnion;   // In order to adapt to dest's handling Onion
+        address destAddress;
+    }
+
     event newTransfer(uint256 indexed txindex, bytes32 hashOnion, address dest, uint256 amount, uint256 fee, uint256 indexed chainId);
     event extract(uint256 txIndex,uint256 amount,bytes32 hashOnion);
 
     function transfer(uint256 chainId,uint256 amount, uint256 fee) external payable;
-    // function extractHashOnionAndBalance() external payable;
+    function extractHashOnion() external;
 }
 
 
 contract SourceContract is ISourceContract{
     using SafeERC20 for IERC20;
 
-    uint256 txIndex;
+    // record transaction
+    mapping(uint256 => DomainStruct) public chainId_Onions; 
+    
+    // TODO For the convenience of demonstration.
+    // in the real case it is 50~100, a relatively low number is used.
+    uint8 public ONEFORK_MAX_LENGTH = 5;
+    
+    // A base fee also needs to be set up for early billing
+    // TODO realy is x = The gas cost of a bond * 1.2 / ONEFORK_MAX_LENGTH
+    uint8 BASE_BIND_FEE = 0;  
+
     // relay need data
     address public tokenAddress;
-    
-    // !!! A base fee also needs to be set up for early billing
-    uint8 BASE_BIND_FEE = 0;  // realy is x
 
-    uint8 public ONEFORK_MAX_LENGTH = 5;
-
-    bytes32 public hashOnion;
-    
-    mapping(uint256 => bytes32) public chainId_Onions; 
-
-    // In order to adapt to dest's handling Onion
-    bytes32 bringHashOnion; 
-    // !!! do it later , if somebody dont want pay fee , will wait 7 day's withdraw time
-    bytes32 noFeeHashOnion; // don't need fast
-    // !!! The data structure also needs to consider multi-layer2 scenarios，multi destination domain
-
-    address public l2Messenger;
-
-    constructor(address _l2Messenger, address _tokenAddress){
-        // !!! no set is safe?
-        // hashOnion = 0x0000000000000000000000000000000000000000000000000000000000000000;
-        l2Messenger = _l2Messenger;
+    constructor(address _tokenAddress){
         tokenAddress = _tokenAddress;
     }
 
     function transfer(uint256 chainId, uint256 amount, uint256 fee) external payable override{
+        require(chainId_Onions[chainId].destContract != "");
+
         uint256 allAmount = amount + fee + BASE_BIND_FEE;
         IERC20(tokenAddress).safeTransferFrom(msg.sender,address(this),allAmount);
 
-        hashOnion = keccak256(abi.encode(hashOnion,keccak256(abi.encode(msg.sender,amount,fee))));
-        txIndex += 1;
+        chainId_Onions[chainId].hashOnion = keccak256(abi.encode(chainId_Onions[chainId].hashOnion,keccak256(abi.encode(msg.sender,amount,fee))));
+        chainId_Onions[chainId].txIndex += 1;
         
-        // !!! Create a portable hashOnion, taking into account the fee reward for the bonder
-        if(txIndex % ONEFORK_MAX_LENGTH == 0) {
-            bringHashOnion = hashOnion;
+        if(chainId_Onions[chainId].txIndex % ONEFORK_MAX_LENGTH == 0) {
+            chainId_Onions[chainId].bringHashOnion = chainId_Onions[chainId].hashOnion;
         }
-        // !!! can delete event function , but less gas , more offchain work
-        emit newTransfer(txIndex,hashOnion,msg.sender,amount,fee,chainId); 
+        
+        emit newTransfer(chainId_Onions[chainId].txIndex,chainId_Onions[chainId].hashOnion,msg.sender,amount,fee,chainId); 
     }
 
     function transferWithDest(uint256 chainId, address dest, uint256 amount, uint256 fee) external payable {
+        require(chainId_Onions[chainId].destContract != "");
+
         uint256 allAmount = amount + fee + BASE_BIND_FEE;
         IERC20(tokenAddress).safeTransferFrom(msg.sender,address(this),allAmount);
-        hashOnion = keccak256(abi.encode(hashOnion,keccak256(abi.encode(dest,amount,fee))));
-        txIndex += 1;
+        chainId_Onions[chainId].hashOnion = keccak256(abi.encode(chainId_Onions[chainId].hashOnion,keccak256(abi.encode(dest,amount,fee))));
+        chainId_Onions[chainId].txIndex += 1;
         
-        // !!! Create a portable hashOnion, taking into account the fee reward for the bonder
-        if(txIndex % ONEFORK_MAX_LENGTH == 0) {
-            bringHashOnion = hashOnion;
+        if(chainId_Onions[chainId].txIndex % ONEFORK_MAX_LENGTH == 0) {
+            chainId_Onions[chainId].bringHashOnion = chainId_Onions[chainId].hashOnion;
         }
-        // !!! can delete event function , but less gas , more offchain work
-        emit newTransfer(txIndex,hashOnion,dest,amount,fee,chainId); 
+        
+        emit newTransfer(chainId_Onions[chainId].txIndex,chainId_Onions[chainId].hashOnion,dest,amount,fee,chainId); 
     }
 
-    function extractHashOnionAndBalance(address raley) external payable {
-    //     // !!! Determine whether the index at the moment is greater than the last withdrawal index, and determine whether the index is at zero
-    //     // !!! Make a settlement for the bond
-    //     // !!! Send onion to Relay, each step will be settled to bonder
-    //     uint256 amount = IERC20(tokenAddress).balanceOf(address(this));
-    //     IERC20(tokenAddress).safeTransfer(relayAddress,amount);
-
-    //     Relay(relayAddress).getHashOnion(txIndex,hashOnion,tokenAddress,msg.sender);
-
-    //     emit extract(txIndex,amount,hashOnion);
-
-
-        // send to L1 dest
-        bytes memory message = abi.encodeWithSignature(
-            "getHashOnion(bytes32)",
-            bringHashOnion
-        );
-
-        IArbSys(l2Messenger).sendTxToL1(
-            raley,
-            message
-        );
+    function extractHashOnion() external {
+        // !!! Create a portable chainId_Onions[chainId].hashOnion, taking into account the fee reward for the bonder
+        // emit extract(chainId_Onions[chainId].txIndex,amount,chainId_Onions[chainId].hashOnion);
     }
 
-    // or send to OP gate on L1
+    function addDestChain(uint256 chainId, address destContract) external {
+        require(chainId_Onions[chainId].destContract == "");
+        
+        chainId_Onions[chainId] = DomainStruct{(
+            destContract: destContract
+        )};
+    }
 }
 
 

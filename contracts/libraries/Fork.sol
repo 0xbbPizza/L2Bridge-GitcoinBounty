836a060ca0269c02c096dd5cf0bff93aab2b9ab7
linkdrone <fengwanqing2014@gmail.com>
2022-05-10 19:03:12 +0800

    update

--- contracts/libraries/Fork.sol
+++ contracts/libraries/Fork.sol
@@ -2,29 +2,6 @@
 
 pragma solidity 0.8.4;
 
-contract NewDestinationContract {
-    using HashOnions for mapping(uint256 => HashOnions.Info);
-    using Fork for mapping(bytes32 => Fork.Info);
-
-    mapping(bytes32 => Fork.Info) public hashOnionForks;
-    mapping(uint256 => HashOnions.Info) public hashOnions;
-    mapping(bytes32 => address) public onionsAddress;
-    mapping(address => bool) private commiterDeposit;
-
-    // uint256 public ONEFORK_MAX_LENGTH = 5; // !!! The final value is 50 , the higher the value, the longer the wait time and the less storage consumption
-    // uint256 DEPOSIT_AMOUNT = 1 * 10**18; // !!! The final value is 2 * 10**17
-
-    function _oneforkMaxLength() internal pure returns (uint8) {
-        // !!! The final value is 50 , the higher the value, the longer the wait time and the less storage consumption
-        return 5;
-    }
-
-    function _depositAmount() internal pure returns (uint256) {
-        // !!! The final value is 2 * 10**17
-        return 1 * 10**18;
-    }
-}
-
 library HashOnions {
     struct Info {
         bytes32 sourceHashOnion;

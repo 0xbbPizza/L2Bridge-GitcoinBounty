6f5ce4e5f2e640153a1045eaa951339058129a4c
linkdrone <fengwanqing2014@gmail.com>
2022-05-30 19:18:04 +0800

    updates

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,28 +1,27 @@
-import { run, ethers } from "hardhat";
-import { BigNumber, Contract} from "ethers";
-
+import { BigNumber } from "ethers";
+import { ethers, run } from "hardhat";
 
 async function main() {
   await run("compile");
   const accounts = await ethers.getSigners();
 
-  const FakeToken = await ethers.getContractFactory("BasicToken")
-  let amount:BigNumber = BigNumber.from(8000000000000000) // no use , the amount is in contract
-  let fakeToken: Contract = await FakeToken.deploy(amount);
-  await fakeToken.deployed()
-  console.log("FakeToken address:",fakeToken.address)
+  const FakeToken = await ethers.getContractFactory("BasicToken");
+  let amount = BigNumber.from(8000000000000000); // no use , the amount is in contract
+  let fakeToken = await FakeToken.deploy(amount);
+  await fakeToken.deployed();
+  console.log("FakeToken address:", fakeToken.address);
 
   // deploy source contract
-  const Source = await ethers.getContractFactory("SourceContract")
-  let source: Contract = await Source.deploy(accounts[0].getAddress(),fakeToken.address)
-  await source.deployed()
-  console.log("sourceContract Address", source.address)
+  const Source = await ethers.getContractFactory("SourceContract");
+  let source = await Source.deploy(accounts[0].getAddress(), fakeToken.address);
+  await source.deployed();
+  console.log("sourceContract Address", source.address);
 
   // deploy dest contract
-  const Dest = await ethers.getContractFactory("DestinationContract")
-  let dest: Contract = await Dest.deploy(fakeToken.address)
-  await dest.deployed()
-  console.log("destContract Address", dest.address)
+  const Dest = await ethers.getContractFactory("NewDestination");
+  let dest = await Dest.deploy(fakeToken.address);
+  await dest.deployed();
+  console.log("destContract Address", dest.address);
 }
 
 main()

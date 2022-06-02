f3e06f5ab76ea089c43c0369fb6e4b865aba365a
linkdrone <fengwanqing2014@gmail.com>
2022-06-02 17:36:46 +0800

    update

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -5,23 +5,49 @@ async function main() {
   await run("compile");
   const accounts = await ethers.getSigners();
 
-  const FakeToken = await ethers.getContractFactory("BasicToken");
-  let amount = BigNumber.from(8000000000000000); // no use , the amount is in contract
-  let fakeToken = await FakeToken.deploy(amount);
-  await fakeToken.deployed();
-  console.log("FakeToken address:", fakeToken.address);
+  const chainId = await accounts[0].getChainId();
+  console.log("chainId", chainId);
 
-  // deploy source contract
-  const Source = await ethers.getContractFactory("SourceContract");
-  let source = await Source.deploy(accounts[0].getAddress(), fakeToken.address);
-  await source.deployed();
-  console.log("sourceContract Address", source.address);
+  let tokenAddress = process.env["TOKEN_ADDRESS"];
+  if (!tokenAddress) {
+    const FakeToken = await ethers.getContractFactory("BasicToken");
+    let amount = BigNumber.from(8000000000000000); // no use , the amount is in contract
+    let fakeToken = await FakeToken.deploy(amount);
+    await fakeToken.deployed();
+    tokenAddress = fakeToken.address;
+  }
+  console.log("Token address:", tokenAddress);
+
+  // Deploy relay
+  const Relay = await ethers.getContractFactory("Relay");
+  const relay = await Relay.deploy();
+  await relay.deployed();
+  console.log("relay Address:", relay.address);
+
+  // Deploy dock
+  const Dock_Mainnet = await ethers.getContractFactory("Dock_MainNet");
+  const dock_Mainnet = await Dock_Mainnet.deploy(relay.address);
+  await dock_Mainnet.deployed();
+  console.log("dock_Mainnet Address:", dock_Mainnet.address);
+
+  // Add dock to relay
+  await relay.addDock(dock_Mainnet.address, chainId);
 
   // deploy dest contract
   const Dest = await ethers.getContractFactory("NewDestination");
-  let dest = await Dest.deploy(fakeToken.address);
+  let dest = await Dest.deploy(tokenAddress, dock_Mainnet.address);
   await dest.deployed();
   console.log("destContract Address", dest.address);
+
+  // deploy source contract
+  const Source = await ethers.getContractFactory("SourceContract");
+  let source = await Source.deploy(
+    tokenAddress,
+    dock_Mainnet.address,
+    dest.address
+  );
+  await source.deployed();
+  console.log("sourceContract Address", source.address);
 }
 
 main()
